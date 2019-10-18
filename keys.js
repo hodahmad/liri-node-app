b9815a708ffcc1503c9ecdb3f8a0c1e879eca17a
Hoda Ahmad <ccs7vp@github.com>
2019-10-17 23:38:19 -0400

    Final changes made to code.

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.d6657ab3d5db4981b04a2e19ea23eb10,
+  secret: process.env.fad4a482673e4c8e92754a665a046daf
 };
